26354c9cef8838cb7653e6ac7d284a5a6bc7d6ee
m8itsjill <jillyusi@icloud.com>
2018-07-12 22:30:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,10 +11,14 @@ end
     
     def name=(new_name)
       @name = new_name
+  end
   
     def breed
       @name = "Beagle"
   end 
 
+    def breed=(new_breed)
+      @breed = new_breed
+    end
   
 end
