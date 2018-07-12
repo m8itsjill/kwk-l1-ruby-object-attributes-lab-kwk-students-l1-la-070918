82ec7d760da7712c5d782ba59caf1e5a2759e9dc
m8itsjill <jillyusi@icloud.com>
2018-07-12 22:33:57 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -9,3 +9,17 @@ def name
   @name = "Beyonce"
 end
 
+def name=(new_name)
+      @name = new_name
+  end
+  
+    def job
+      @job = ""
+  end 
+
+    def breed=(new_breed)
+      @breed = new_breed
+    end
+  
+end
+
